9267a908e309326c7a39b75657ecb0658c978c8e
MacKenzie Bulkowski-Rose <mackenzie.bulkowski.rose@gmail.com>
2020-03-03 11:44:18 -0500

    Add css styling to user inputs

--- js/main.js
+++ js/main.js
@@ -13,12 +13,12 @@ pastVerb = prompt('A past tense verb (ran, burped, etc.)');
 story = prompt('Which story would you like?\na) Snacking\nb) Escaping\nc) Winning\n\nEnter the letter below:');
 
 switch (story) {
-  case 'a' : document.write(`After snacking on ${adjective} treats, the ${colour} bellied ${creature} ${pastVerb} for hours.`);
+  case 'a' : document.write(`After snacking on <span>${adjective}</span> treats, the <span>${colour}</span> bellied <span>${creature}</span> <span>${pastVerb}</span> for hours.`);
     break;
 
-  case 'b' : document.write(`Amelia ${pastVerb} through the ${adjective} ${colour} nebula escaping the space ${creature}.`);
+  case 'b' : document.write(`Amelia <span>${pastVerb}</span> through the <span>${adjective}</span> <span>${colour}</span> nebula escaping the space <span>${creature}</span>.`);
     break;
 
-  case 'c' : document.write(`Jackson chose his ${adjective}, ${colour} ${creature} card and ${pastVerb} it to the table knowing he won.`);
+  case 'c' : document.write(`Jackson chose his <span>${adjective}</span>, <span>${colour}</span> <span>${creature}</span> card and <span>${pastVerb}</span> it to the table knowing he won.`);
     break;
 }
